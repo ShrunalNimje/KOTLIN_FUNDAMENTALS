a389f123388753d52091b0e37b1d3995cb975ca1
Shrunal Nimje <shrunalnimje@gmail.com>
2024-01-09 19:35:08 +0530

    update in functions

--- app/src/main/java/com/mycode/kotlinfundamentals/lec10_function.kt
+++ app/src/main/java/com/mycode/kotlinfundamentals/lec10_function.kt
@@ -2,6 +2,18 @@ package com.mycode.kotlinfundamentals
 
 // functions in Kotlin
 
+fun coffeeMachine(sugar : Int, name : String){
+    if (sugar == 1 || sugar == 0){
+        println("$sugar spoon of sugar added for $name")
+    }
+    else if (sugar < 0){
+        println("$name ! \nPlease put the valid amt of sugar")
+    }
+    else{
+        println("$sugar spoons of sugar added for $name")
+    }
+}
+
 fun myFunction(){
     println("This is my custom function")
 }
@@ -33,7 +45,17 @@ fun main() {
     // here 23 & 56 are two attributes
     val add = add(23,56)
     println("Addition is $add")
-    // here 23.75 & 45.0 are two attributes
-    println("Average of two number is ${avg(23.75,45.0)}")
+
+    println("Enter your two numbers to calculate an average ")
+    val num1 = readln().toDouble()
+    val num2 = readln().toDouble()
+    println("Average of two number is ${avg(num1,num2)}")
+
+    println("For whom this coffee made for ?")
+    val name = readln()
+    println("how much no. of spoon of sugar you want to add in coffee ? ")
+    val sugar = readln().toInt()
+    // getting input from user in function
+    coffeeMachine(sugar, name)
 
 }
